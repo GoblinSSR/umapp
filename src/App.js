e559cdc118ffe947ba49060ba7c88f4819f1960f
cheng <cheng@chengdeMacBook-Pro.local>
2021-12-18 23:00:32 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Title from './components/title';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+		  <p>
+		  Edit <code>src/App.js</code> and save to reload.
+		  </p>
+		  <Title >
+			umapp
+		  </Title>
+	  </header>
+	</div>
+  );
+}
+
+export default App;
